e5f9b8bd7c5973066189acca8406cb8ea9915ac8
Zach Fechko <zachary.fechko@wsu.edu>
2024-05-23 16:27:08 -0700

    Changed name to specify between cmd app and qt app, this will be changed back

---
+++ job_tracker_cmd.py
@@ -0,0 +1,142 @@
+"""
+This project will start as a basic command line tool to track job applications 
+with a Postgres SQL database.
+
+I intend to create a GUI for this project
+
+
+The project will have the following features:
+- Add a position to the database
+- Edit a position
+- Display all positions
+- Display positions through filters
+- Utilize the rich library for a better command line interface
+"""
+
+import psycopg2
+import pandas as pd
+from rich.console import Console
+from rich.table import Column, Table
+from datetime import date
+from os import system, name
+from pick import pick
+
+def clear_screen():
+    """
+    Clears the screen
+    """
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
+
+def execute_query(query):
+    """
+    Connects to and executes a query on the database
+    """
+    c = Console()
+    try:
+        conn = psycopg2.connect(host = 'localhost', dbname = 'jobs', user='postgres', password='zfechko')
+        cursor = conn.cursor()
+        c.print('[bold green]Connected to the database[/bold green]')
+    except:
+        c.print('[bold red]Failed to connect to the database[/bold red]')
+        exit()
+    cursor = conn.cursor()
+    cursor.execute(query)
+    conn.commit()
+    if 'SELECT' in query:
+        return cursor.fetchall()
+    else:
+        c.print('[bold green]Changes made successfully[/bold green]')
+
+
+def add_listing():
+    """
+    Adds a listing to the database
+    """
+    clear_screen()
+    c = Console()
+    c.print('[bold]Adding a listing[/bold]')
+    title = input('Job Title: ').title()
+    company = input('Company Name: ')
+    salary = input('Lower value of salary range: ')
+    city = input('City: ').title()
+    state = input('State: ').upper()
+    job_type = input('Job Type (In Person, Remote, Hybrid): ').title()
+    applied_on = date.today()
+    query = f"INSERT INTO POSITION (title, company, salary, job_city, job_state, job_type, applied_on) VALUES ('{title}', '{company}', {salary}, '{city}', '{state}', '{job_type}', '{applied_on}');"
+    execute_query(query)
+    pass
+
+def update_listing():
+    """
+    Updates an attribute of an existing listing
+
+    - Prompts the user to input the ID of a listing
+    - Shows the user the current attributes of the listing and prompts for which attribute to update
+    - User inputs new attribute value and the listing in the DB is updated
+    """
+    clear_screen()
+    c = Console()
+    display_listings()
+    id = input('Enter the ID of the listing you want to update: ')
+    options = ['Job Title', 'Company', 'Salary', 'City', 'State', 'Job Type', 'Application Date', 'Application Status']
+    title = "What would you like to update?"
+    option, index = pick(options, title, indicator='->')
+    new_value = input('Enter the new value: ')
+    index_to_attribute_dict = {0: 'title', 1: 'company', 2: 'salary', 3: 'job_city', 4: 'job_state', 5: 'job_type', 6: 'applied_on', 7: 'status'}
+    query = f"UPDATE POSITION SET {index_to_attribute_dict[index]} = '{new_value}' WHERE id = {id};"
+    execute_query(query)
+
+def display_listings():
+    """
+    Displays all listings in the database
+
+    - Displays all listings in the database using rich table
+    - Shows the user the number of listings in the database
+    """
+    clear_screen()
+    query = f"SELECT * FROM POSITION ORDER BY ID ASC;"
+    result = execute_query(query)
+    table = Table(
+        "ID",
+        "Job Title",
+        "Company",
+        Column(header="Salary", style="green"),
+        "City",
+        "State",
+        "Job Type",
+        "Applied On",
+        "Application Status",
+        title='Jobs I\'ve Applied To')
+
+    for row in result:
+        table.add_row(str(row[0]), row[1], row[2], row[3], row[4], row[5], row[6], str(row[7]), row[8])
+
+    c = Console()
+    c.print(table)
+
+
+
+
+if __name__ == '__main__':
+    ended = False
+    while not ended:
+        clear_screen()
+        title = 'Select an option: '
+        options = ['Add an application', 'Update an application', 'Display all applications', 'Exit']
+        option, index = pick(options, title, indicator='->')
+        if index == 0:
+            add_listing()
+        elif index == 1:
+            update_listing()
+        elif index == 2:
+            display_listings()
+            input()
+        elif index == 3:
+            ended = True
+            clear_screen()
+            c = Console()
+            c.print('[bold]Goodbye![/bold]')
+            exit()
